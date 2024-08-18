ff8256bcc7e54206b0e8460f72e4d9df4cc55776
iamrajeev2005 <fea.rajeev@gmail.com>
2024-08-18 22:22:26 +0530

    all done

---
+++ script.js
@@ -0,0 +1,149 @@
+import * as THREE from "three";
+import {
+  FontLoader,
+  OrbitControls,
+  TextGeometry,
+} from "three/examples/jsm/Addons.js";
+import "./style.css";
+// import gsap from "gsap";
+import GUI from "lil-gui";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector(".webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("./static/textures/matcaps/3.png");
+const matcapDonutTexture = textureLoader.load(
+  "./static/textures/matcaps/1.png"
+);
+
+/**
+ * Fonts Loader
+ */
+const fontLoader = new FontLoader();
+fontLoader.load("./static/fonts/gentilis_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Rajeev's World", {
+    font: font,
+    size: 0.5,
+    depth: 0.2,
+    curveSegments: 6,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+  textGeometry.center();
+  // textGeometry.computeBoundingBox()
+  // textGeometry.translate(
+  //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
+  // )
+  const material = new THREE.MeshMatcapMaterial();
+  material.matcap = matcapTexture;
+  // textMaterial.wireframe = true
+  const text = new THREE.Mesh(textGeometry, material);
+  scene.add(text);
+//   console.time("donut");
+
+  const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+  for (let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, material);
+
+    donut.position.x = (Math.random() - 0.5) * 10;
+    donut.position.y = (Math.random() - 0.5) * 10;
+    donut.position.z = (Math.random() - 0.5) * 10;
+
+    donut.rotation.x = Math.random() * Math.PI * 2;
+    donut.rotation.y = Math.random() * Math.PI * 2;
+
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
+//   console.timeEnd("donut");
+});
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
